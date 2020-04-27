7efc8b9af3b63a7cd71222f2a9bc13958a6e7ae7
BryanAnariba <43623601+BryanAnariba@users.noreply.github.com>
2020-04-27 09:53:44 -0600

    c#

---
+++ codigo-poo/uso-clases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uso_clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Works Class in POO");
+        }
+    }
+}
